560caaf2eee01f1211f310d59e29374e5755e005
sipergod <siperkingvip1811@gmail.com>
2022-07-07 17:00:55 +0700

    fix build v3

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -33,13 +33,17 @@ export const Layout = ({
 	const appState = useAppState();
 	const appStateDispatch = useAppStateDispatch();
 
-	const [innerHeight] = useState<number>(window.innerHeight);
+	const [innerHeight, setInnerHeight] = useState<number>();
 
 	useEffect(() => {
 		if (!listTabBar || listTabBar.length === 0) {
 			appStateDispatch({
 				type: "setTabActive",
 			});
+		}
+
+		if (!innerHeight) {
+			setInnerHeight(window.innerHeight);
 		}
 	}, [listTabBar, appStateDispatch]);
 
@@ -68,7 +72,9 @@ export const Layout = ({
 							? "4rem"
 							: "3.25rem",
 					marginBottom: hasBottomTabBar ? "4rem" : "0",
-					minHeight: `calc(${innerHeight}px - ${
+					minHeight: `calc(${
+						innerHeight ? `${innerHeight}px` : "100vh"
+					} - ${
 						listTabBar && listTabBar.length > 0
 							? "calc(5.75rem + 1px)"
 							: appBarTitle === undefined || appBarTitle === ""

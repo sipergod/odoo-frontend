e4b1aa50a1a0a1a8ca536c8c9da88d37353de499
sipergod <siperkingvip1811@gmail.com>
2022-07-07 14:36:11 +0700

    update render page by tab

--- src/pages/content.tsx
+++ src/pages/content.tsx
@@ -1,19 +1,39 @@
 import React from "react";
+import { useAppState } from "../components/appContext/AppContext";
 import { Layout } from "../components/layout/layout";
+import { SimpleTimeline } from "../components/timeline/simpleTimeline";
+
+const ListTabBar = ["Messages", "Files", "Pages"];
 
 const ContentPage = (): JSX.Element => {
+	const appState = useAppState();
+
 	return (
 		<>
 			<main>
 				<Layout
 					searchPlaceHolderByTab
-					listTabBar={["Messages", "Files", "Pages"]}
+					listTabBar={ListTabBar}
 					hasFloatingButton
 					hasBottomTabBar
 					bottomTabBarIndex={1}
 				>
 					<div className="column">
 						<p>Content page</p>
+
+						{appState.activeTab === ListTabBar[0].toLowerCase() && (
+							<div className="block"></div>
+						)}
+
+						{appState.activeTab === ListTabBar[1].toLowerCase() && (
+							<div className="block">
+								<SimpleTimeline />
+							</div>
+						)}
+
+						{appState.activeTab === ListTabBar[2].toLowerCase() && (
+							<div className="block"></div>
+						)}
 					</div>
 				</Layout>
 			</main>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,40 @@
 import React from "react";
 import { SimpleTimeline } from "../components/timeline/simpleTimeline";
 import { Layout } from "../components/layout/layout";
+import { useAppState } from "../components/appContext/AppContext";
+
+const ListTabBar = ["All clients", "Team", "Groups"];
 
 const IndexPage = (): JSX.Element => {
+	const appState = useAppState();
+
 	return (
 		<>
 			<main>
 				<Layout
 					searchPlaceHolder={`clients & phonebook...`}
-					listTabBar={["All clients", "Team", "Groups"]}
+					listTabBar={ListTabBar}
 					hasFloatingButton
 					hasBottomTabBar
 					bottomTabBarIndex={0}
 				>
 					<div className="column">
-						<p>Odoo frontend</p>
+						<p>Odoo frontend {appState.activeTab}</p>
 					</div>
-					<SimpleTimeline />
+
+					{appState.activeTab === ListTabBar[0].toLowerCase() && (
+						<div className="block">
+							<SimpleTimeline />
+						</div>
+					)}
+
+					{appState.activeTab === ListTabBar[1].toLowerCase() && (
+						<div className="block"></div>
+					)}
+
+					{appState.activeTab === ListTabBar[2].toLowerCase() && (
+						<div className="block"></div>
+					)}
 				</Layout>
 			</main>
 		</>
